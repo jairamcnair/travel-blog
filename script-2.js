b5ac4c6f26cd681346a2a7abbb7891301f0f8b0a
Jaira McNair <jaimcnair10@gmail.com>
2025-12-05 17:01:07 -0500

    updates

--- script-2.js
+++ script-2.js
@@ -1,9 +1,7 @@
 
 
 const jumpToItinerary = document.querySelector('jumpToItinerary');
-const itinerary = document.getElementById('itinerary');
-
-
+const itinerary = document.querySelector('itinerary');
 function scrollToItinerary() {
   itinerary.scrollIntoView({
     behavior: 'smooth', // 'auto' (default), 'smooth', or 'instant'
@@ -11,5 +9,16 @@ function scrollToItinerary() {
     inline: 'nearest'   // 'start', 'center', 'end', or 'nearest' (default)
 });}
 jumpToItinerary.addEventListener('click', scrollToItinerary);
+
+
+const jumpToIntro = document.querySelector('jumpToIntro');
+const intro = document.querySelector('intro');
+function scrollToIntro() {
+  intro.scrollIntoView({
+    behavior: 'smooth', // 'auto' (default), 'smooth', or 'instant'
+    block: 'start',     // 'start' (default), 'center', 'end', or 'nearest'
+    inline: 'nearest'   // 'start', 'center', 'end', or 'nearest' (default)
+});}
+jumpToIntro.addEventListener('click', scrollToIntro);
 
 
